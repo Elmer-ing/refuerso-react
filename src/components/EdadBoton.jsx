3ba5e1bdf523e625fc6d3cfbaba6bda67e600c90
Elmer-ing <emosquerada@cesde.net>
2025-04-02 15:13:33 -0500

    codigo fucional-basico

--- src/components/EdadBoton.jsx
+++ src/components/EdadBoton.jsx
@@ -1,10 +1,22 @@
 import '../styles/EdadBoton.css'
+import { useState } from 'react';
 
 function EdadBoton() {
-    return(
-        <div className="contenedor">
-            
+  
+  const [edad, setEdad] = useState(21);
+
+  return(
+    <div className="contenedor">
+      <div className='eb-item'>
+        <div className='eb-item__info'>
+          <p className='eb-item__info-nombre'>Nombre: Elmer Mosquera</p>
+          <p className='eb-item__info-edad'>Edad: {edad}</p>
         </div>
-    )
+        <div className='eb-item__botones'>
+          <button className='eb-item__boton' onClick={()=> setEdad(edad +1)}>Aumentar edad</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default EdadBoton
