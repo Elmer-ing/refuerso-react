4767bd87edf49bc2d41fff9358097c1199abb707
Elmer-ing <emosquerada@cesde.net>
2025-04-02 22:50:56 -0500

    reestructura de contenedor de items (EdadBoton)

--- src/components/EdadBoton.jsx
+++ src/components/EdadBoton.jsx
@@ -1,21 +1,50 @@
 import '../styles/EdadBoton.css'
-import { useState } from 'react';
+import React from 'react';
+// import { useState } from 'react';
+import Item from './Item';
 
 function EdadBoton() {
   
-  const [edad, setEdad] = useState(21);
+  // const [edad, setEdad] = useState(21);
+
+  const personasItems = [
+    {
+      id: 1,
+      nombre: 'Elmer Mosquera',
+      edad: 22
+    },
+    {
+      id: 2,
+      nombre: 'Tomas Gomez',
+      edad: 17
+    },
+    {
+      id: 3,
+      nombre: 'Andres Pino',
+      edad: 18
+    },
+    {
+      id: 4,
+      nombre: 'Andres Sotelo',
+      edad: 19
+    },
+    {
+      id: 5,
+      nombre: 'Sebastian Monterrosa',
+      edad: 21
+    },
+  ]
 
   return(
     <div className="contenedor">
-      <div className='eb-item'>
-        <div className='eb-item__info'>
-          <p className='eb-item__info-nombre'>Nombre: Elmer Mosquera</p>
-          <p className='eb-item__info-edad'>Edad: {edad}</p>
-        </div>
-        <div className='eb-item__botones'>
-          <button className='eb-item__boton' onClick={()=> setEdad(edad +1)}>Aumentar edad</button>
-        </div>
-      </div>
+      {personasItems.map((persona)=>(
+        <Item
+          key={persona.id}
+          nombre={persona.nombre} 
+          edad={persona.edad}
+        />
+      ))}
+
     </div>
   )
 }
